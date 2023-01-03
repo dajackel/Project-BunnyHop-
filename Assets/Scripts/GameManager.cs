03e342450efd2fd07fc9426a8b8f13e67fbd3b9a
DAJackel <daniel.jackson020702@gmail.com>
2023-01-03 10:50:15 -0600

    Commented out ad related code as it's not needed for current sprint.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour, IUnityAdsInitializationListener
+public class GameManager : MonoBehaviour//, IUnityAdsInitializationListener
 {
-    const string _androidGameId = "",
-       bannerAd = "Main_Game_banner", FullScreenAd = "Pre-respawn_FS_ad";
+    const string androidGameId = "",iosGameId = "",
+       androidBannerUnit = "Banner_Android", iosBannerUnit = "Banner_iOS", FullScreenAd = "Pre-respawn_FS_ad";
     [SerializeField] Button _loadBannerButton,_showBannerButton,_hideBannerButton;
     [SerializeField] BannerPosition _bannerPosition = BannerPosition.BOTTOM_CENTER;
     bool _testMode = true;
-    private string _gameId;
+    private string adUnitId;
     public bool paused = false;
     private float timeScale = 1, currentLevelPos=0;
     public GameObject[] levelSection;
@@ -20,99 +20,102 @@ public class GameManager : MonoBehaviour, IUnityAdsInitializationListener
 
     private void Awake()
     {
-        
-    }
-    #region BANNERADS
-    public void InitializeAds()
-    {
-        Advertisement.Initialize(_gameId, _testMode, this);
-    }
-    public void OnInitializationComplete()
-    {
-        Debug.Log("Unity Ads initialization complete.");
-    }
-    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
-    {
-        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
-    }
-    // Implement code to execute when the loadCallback event triggers:
-    void OnBannerLoaded()
-    {
-        Debug.Log("Banner loaded");
-
-        // Configure the Show Banner button to call the ShowBannerAd() method when clicked:
-        _showBannerButton.onClick.AddListener(ShowBannerAd);
-        // Configure the Hide Banner button to call the HideBannerAd() method when clicked:
-        _hideBannerButton.onClick.AddListener(HideBannerAd);
-
-        // Enable both buttons:
-        _showBannerButton.interactable = true;
-        _hideBannerButton.interactable = true;
+        adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+           ? iosBannerUnit
+           : androidBannerUnit;
     }
 
-    // Implement code to execute when the load errorCallback event triggers:
-    void OnBannerError(string message)
-    {
-        Debug.Log($"Banner Error: {message}");
-        // Optionally execute additional code, such as attempting to load another ad.
-    }
+//    #region BANNERADS
+//    public void InitializeAds()
+//    {
+//        Advertisement.Initialize(_gameId, _testMode, this);
+//    }
+//    public void OnInitializationComplete()
+//    {
+//        Debug.Log("Unity Ads initialization complete.");
+//    }
+//    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+//    {
+//        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+//    }
+//    // Implement code to execute when the loadCallback event triggers:
+//    void OnBannerLoaded()
+//    {
+//        Debug.Log("Banner loaded");
 
-    // Implement a method to call when the Show Banner button is clicked:
-    void ShowBannerAd()
-    {
-        // Set up options to notify the SDK of show events:
-        BannerOptions options = new BannerOptions
-        {
-            clickCallback = OnBannerClicked,
-            hideCallback = OnBannerHidden,
-            showCallback = OnBannerShown
-        };
+//        // Configure the Show Banner button to call the ShowBannerAd() method when clicked:
+//        _showBannerButton.onClick.AddListener(ShowBannerAd);
+//        // Configure the Hide Banner button to call the HideBannerAd() method when clicked:
+//        _hideBannerButton.onClick.AddListener(HideBannerAd);
 
-        // Show the loaded Banner Ad Unit:
-        Advertisement.Banner.Show(bannerAd, options);
-    }
+//        // Enable both buttons:
+//        _showBannerButton.interactable = true;
+//        _hideBannerButton.interactable = true;
+//    }
 
-    // Implement a method to call when the Hide Banner button is clicked:
-    void HideBannerAd()
-    {
-        // Hide the banner:
-        Advertisement.Banner.Hide();
-    }
+//    // Implement code to execute when the load errorCallback event triggers:
+//    void OnBannerError(string message)
+//    {
+//        Debug.Log($"Banner Error: {message}");
+//        // Optionally execute additional code, such as attempting to load another ad.
+//    }
 
-    void OnBannerClicked() { }
-    void OnBannerShown() { }
-    void OnBannerHidden() { }
+//    // Implement a method to call when the Show Banner button is clicked:
+//    void ShowBannerAd()
+//    {
+//        // Set up options to notify the SDK of show events:
+//        BannerOptions options = new BannerOptions
+//        {
+//            clickCallback = OnBannerClicked,
+//            hideCallback = OnBannerHidden,
+//            showCallback = OnBannerShown
+//        };
 
-    void OnDestroy()
-    {
-        // Clean up the listeners:
-        _loadBannerButton.onClick.RemoveAllListeners();
-        _showBannerButton.onClick.RemoveAllListeners();
-        _hideBannerButton.onClick.RemoveAllListeners();
-    }
-    public void LoadBanner()
-    {
-        // Set up options to notify the SDK of load events:
-        BannerLoadOptions options = new BannerLoadOptions
-        {
-            loadCallback = OnBannerLoaded,
-            errorCallback = OnBannerError
-        };
+//        // Show the loaded Banner Ad Unit:
+//        Advertisement.Banner.Show(bannerAd, options);
+//    }
 
-        // Load the Ad Unit with banner content:
-        Advertisement.Banner.Load(bannerAd, options);
-    }
-#endregion
+//    // Implement a method to call when the Hide Banner button is clicked:
+//    void HideBannerAd()
+//    {
+//        // Hide the banner:
+//        Advertisement.Banner.Hide();
+//    }
+
+//    void OnBannerClicked() { }
+//    void OnBannerShown() { }
+//    void OnBannerHidden() { }
+
+//    void OnDestroy()
+//    {
+//        // Clean up the listeners:
+//        _loadBannerButton.onClick.RemoveAllListeners();
+//        _showBannerButton.onClick.RemoveAllListeners();
+//        _hideBannerButton.onClick.RemoveAllListeners();
+//    }
+//    public void LoadBanner()
+//    {
+//        // Set up options to notify the SDK of load events:
+//        BannerLoadOptions options = new BannerLoadOptions
+//        {
+//            loadCallback = OnBannerLoaded,
+//            errorCallback = OnBannerError
+//        };
+
+//        // Load the Ad Unit with banner content:
+//        Advertisement.Banner.Load(bannerAd, options);
+//    }
+////#endregion
 
     void Start()
     {
-        _showBannerButton.interactable = false;
-        _hideBannerButton.interactable=false;
+        //_showBannerButton.interactable = false;
+        //_hideBannerButton.interactable=false;
 
-        Advertisement.Banner.SetPosition(_bannerPosition);
+        //Advertisement.Banner.SetPosition(_bannerPosition);
 
-        _loadBannerButton.onClick.AddListener(LoadBanner);
-        _loadBannerButton.interactable = true;
+        //_loadBannerButton.onClick.AddListener(LoadBanner);
+        //_loadBannerButton.interactable = true;
 
     }
 
