678ba0507027f253e7ee209e7251482bb93ebdba
DAJackel <daniel.jackson020702@gmail.com>
2023-01-04 11:59:38 -0600

    Changed camera to work going forward with new direction. Edited camera to work with aspect ratios. Started adding android dependencies.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,25 +7,46 @@ public class CameraScript : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject Player, RLevelBound, LLevelBound;
     public Vector2 curVelocity;
-    private float maxXDist, minXDist, minYDist;
-    private float screenWidth, screenHeight, screenRatio;
+    private float minYDist = 6.67f;
     void Start()
     {
-        //set player control limits to screen width
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;
-        screenRatio = screenWidth / screenHeight; //this determines the screen ratio (3:4=iPad_Tall, 9:16=iPhone5_Tall, etc)
-
-        maxXDist = RLevelBound.transform.position.x - RLevelBound.transform.localScale.x/2 - (screenRatio * Camera.main.orthographicSize * 2) / 2;//minus widthof camera
-        minXDist = LLevelBound.transform.position.x + LLevelBound.transform.localScale.x/2+ (screenRatio * Camera.main.orthographicSize  * 2)/2;//plus widthofcamera
-
+        switch (Camera.main.aspect)
+        {
+            case 480.0f / 800.0f:
+                Camera.main.orthographicSize = 17.87372f;
+                //18.00567
+                break;
+            case 0.5008292f:
+                print(Camera.main.aspect);
+                print("18:9");
+                //21.56672
+                break;
+            case 720.0f / 1280.0f:
+                print(Camera.main.aspect);
+                Camera.main.orthographicSize = 19.11508f;
+                break;
+            case 1440.0f / 2960.0f:
+                Camera.main.orthographicSize = 22.14385f;
+                break;
+            case 1080.0f / 2160.0f:
+                Camera.main.orthographicSize = 21.57014f;
+                break;
+            default:
+                if (Camera.main.aspect - 0.5008292f < 0)
+                    Camera.main.orthographicSize = 21.56672f;
+                else
+                    Camera.main.orthographicSize = 19.13686f;
+                print(Camera.main.aspect);
+                print("default");
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(Player.transform.position.x,minXDist,maxXDist),
-                                         Mathf.Clamp(Player.transform.position.y,minYDist, Player.transform.position.y),
+        transform.position = new Vector3(/*Mathf.Clamp(Player.transform.position.x,minXDist,maxXDist)*/0.0f,
+                                         Mathf.Clamp(Player.transform.position.y, minYDist, Player.transform.position.y),
                                          transform.position.z);
     }
 }
