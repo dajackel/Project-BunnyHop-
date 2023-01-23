6e085d69c1d77b01fe2c9db48dd3f0133c8051d9
DAJackel <daniel.jackson020702@gmail.com>
2023-01-23 16:15:45 -0600

    Added new muted audio sprite. Added highscore reset button to main menu. Added reset confirmation window to main menu.

--- Assets/Scripts/MainMenuUIScript.cs
+++ Assets/Scripts/MainMenuUIScript.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenuUIScript : MonoBehaviour
 {
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Sprite audioON,audioOFF;
     public void playGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+    }
+    public void ToggleAudio()
+    {
+        float vol;
+        mixer.GetFloat("MasterVolume", out vol);
+        mixer.SetFloat("MasterVolume", (vol == -80.0f) ? 0.0f : -80.0f);
+            Image audioSprite = GetComponentsInChildren<Image>()[1];
+        if (vol == 0)
+            audioSprite.sprite = audioOFF;
+        else
+            audioSprite.sprite = audioON;
+    }
+    public void TriggerHighscoreResetWindow()
+    {
+
+    }
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
     }
 }
